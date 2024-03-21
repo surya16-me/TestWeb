d64b192916578557e6089c0550baac8ca64fdb2d
surya16-me <suryaadjie08@gmail.com>
2024-03-21 21:51:37 +0700

    Add project files.

---
+++ TestWeb/Controllers/SalesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestWeb.Controllers
+{
+    public class SalesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
